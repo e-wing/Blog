bd4e3cbf5ebffe365d51df372f94c9628753698a
e-wing <emily@ouroctopus.com>
2016-06-27 22:06:54 -0700

    cleaning

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += ['plugins/revolution/extensions/*']
 
 Rails.application.config.assets.precompile += ['plugins/revolution/*']
 Rails.application.config.assets.precompile += ['plugins/revolution/placeholders/*']
